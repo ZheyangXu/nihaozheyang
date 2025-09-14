ab280317d0ef938be00eb003b4041c53159995f2
ZheyangXu <jishengxzy@hotmail.com>
2025-09-14 13:47:40 +0800

    cicd: add nginx config

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div id="app" style="display:flex;min-height:100vh;flex-direction:column;">
     <main style="flex:1;display:flex;align-items:center;justify-content:center;font-size:32px;">
       你好晢旸
+      <br/>
+      寻寻觅觅, 冷冷清清, 凄凄惨惨戚戚.
     </main>
     <SiteFooter />
   </div>
